7ea6c8480a4af8156b854ea844cb0921c1355a65
Mike Allanson <michael.allanson@gmail.com>
2016-07-27 10:08:57 +0100

    Add tests for `/lint` route

--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,13 @@ const favicon = require("serve-favicon")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 const path = require("path")
-const lint = require("./src/server/lint")
-const errorHandler = require("./src/server/error-handler")
+const lint = require("./lint")
+const errorHandler = require("./error-handler")
 
 const NODE_ENV = process.env.NODE_ENV
-const PORT = process.env.PORT || 8080
+
 const app = express()
-const faviconPath = path.join(__dirname, "src/static/favicon.ico")
+const faviconPath = path.join(__dirname, "../static/favicon.ico")
 
 app.locals.settings["x-powered-by"] = false
 app.use(favicon(faviconPath))
@@ -19,14 +19,13 @@ app.use(compression())
 app.use(bodyParser.json())
 
 if (NODE_ENV === "development") {
-  const developmentMiddleware = require("./src/server/development-middleware")
+  const developmentMiddleware = require("./development-middleware")
   app.use(developmentMiddleware.dev)
   app.use(developmentMiddleware.hot)
 }
 
 app.post("/lint", lint)
-app.use(express.static(path.join(__dirname, "dist"), { maxAge: "1 year" }))
+app.use(express.static(path.join(__dirname, "../../dist"), { maxAge: "1 year" }))
 app.use(errorHandler)
-app.listen(PORT, () => {
-  console.log(`Server started on ${PORT} in ${NODE_ENV}`) // eslint-disable-line no-console
-})
+
+module.exports = app
