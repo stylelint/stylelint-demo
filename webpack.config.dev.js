0fa60d0e9f347c4b4919d85321d5c90f16dd218f
Mike Allanson <michael.allanson@gmail.com>
2017-03-02 23:20:48 +0000

    Update webpack to version 2 and simplify CSS config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,28 +4,20 @@ const webpack = require("webpack")
 const prodConfig = require("./webpack.config.prod.js")
 
 module.exports = {
-  debug: true,
   entry: [
     "webpack-hot-middleware/client?reload=true",
     prodConfig.entry,
   ],
   output: prodConfig.output,
   module: prodConfig.module,
-  postcss(webpack) {
-    return [
-      require("postcss-import")({ addDependencyTo: webpack }),
-      require("postcss-cssnext")(),
-    ]
-  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       inject: "body",
       template: "src/client/index.ejs",
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.EnvironmentPlugin(["NODE_ENV"]),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
 }
