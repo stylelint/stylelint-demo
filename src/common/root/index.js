stylelint/stylelint-demo

commit d87354f5029c4f884319075fe7204ccfae8ba66b

Refactor from classes to hooks (#141)

src/common/root/index.js
@@ -1,5 +1,5 @@
 /* global fetch:false */
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import debounce from "lodash.debounce";
 import Linter from "../linter";
 import recommendedConfig from "stylelint-config-recommended";
@@ -7,32 +7,18 @@ import standardConfig from "stylelint-config-standard";
 import "whatwg-fetch";
 
 const defaultCSS = "a {color: #FFF; }\n";
-const config = {
+const defaultConfig = {
   rules: Object.assign(recommendedConfig.rules, standardConfig.rules)
 };
 
-export default class Root extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: defaultCSS,
-      config: JSON.stringify(config, null, 2),
-      syntax: "css",
-      warnings: [],
-      error: false
-    };
-    this.lint = debounce(this.lint, 250);
-    this.lint = this.lint.bind(this);
-    this.onCodeChange = this.onCodeChange.bind(this);
-    this.onConfigChange = this.onConfigChange.bind(this);
-    this.onSyntaxChange = this.onSyntaxChange.bind(this);
-  }
+export default function Root() {
+  const [code, setCode] = useState(defaultCSS);
+  const [config, setConfig] = useState(JSON.stringify(defaultConfig, null, 2));
+  const [syntax, setSyntax] = useState("css");
+  const [warnings, setWarnings] = useState([]);
+  const [error, setError] = useState(false);
 
-  componentDidMount() {
-    this.lint();
-  }
-
-  lint() {
+  function lint() {
     fetch("/lint", {
       method: "POST",
       headers: {
@@ -40,9 +26,9 @@ export default class Root extends Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        code: this.state.code,
-        config: this.state.config,
-        syntax: this.state.syntax
+        code,
+        config,
+        syntax
       })
     })
       .then(response => {
@@ -50,62 +36,35 @@ export default class Root extends Component {
       })
       .then(data => {
         if (data.error) {
-          this.setState({
-            error: data.error
-          });
+          setError(data.error);
         } else {
-          this.setState({
-            warnings: data.warnings,
-            error: false
-          });
+          setWarnings(data.warnings);
+          setError(false);
         }
       })
       .catch(error => {
-        this.setState({
-          error: `Unable to lint CSS: \n\n ${error}`
-        });
+        setError(`Unable to lint CSS: \n\n ${error}`);
       });
   }
 
-  onCodeChange(code) {
-    this.setState(
-      {
-        code
-      },
-      this.lint
-    );
-  }
+  useEffect(() => {
+    debounce(lint, 250)();
+  });
 
-  onConfigChange(config) {
-    this.setState(
-      {
-        config
-      },
-      this.lint
-    );
-  }
-
-  onSyntaxChange(syntax) {
-    this.setState(
-      {
-        syntax
-      },
-      this.lint
-    );
-  }
-
-  render() {
-    return (
-      <Linter
-        onCodeChange={this.onCodeChange}
-        onConfigChange={this.onConfigChange}
-        onSyntaxChange={this.onSyntaxChange}
-        code={this.state.code}
-        config={this.state.config}
-        syntax={this.state.syntax}
-        warnings={this.state.warnings}
-        error={this.state.error}
-      />
-    );
-  }
+  return (
+    <Linter
+      onCodeChange={input => {
+        setCode(input);
+      }}
+      onConfigChange={input => {
+        setConfig(input);
+      }}
+      onSyntaxChange={setSyntax}
+      code={code}
+      config={config}
+      syntax={syntax}
+      warnings={warnings}
+      error={error}
+    />
+  );
 }

commit 096325fdea477f92a14dfb118c4f50ea93fd2a1a

Fix /lint spam regression after #141

src/common/root/index.js
@@ -1,10 +1,10 @@
 /* global fetch:false */
 import React, { useState, useEffect } from "react";
-import debounce from "lodash.debounce";
 import Linter from "../linter";
 import recommendedConfig from "stylelint-config-recommended";
 import standardConfig from "stylelint-config-standard";
 import "whatwg-fetch";
+import useDebouncedCallback from "use-debounce/lib/callback";
 
 const defaultCSS = "a {color: #FFF; }\n";
 const defaultConfig = {
@@ -47,9 +47,13 @@ export default function Root() {
       });
   }
 
+  const debouncedLint = useDebouncedCallback(() => {
+    lint();
+  }, 250);
+
   useEffect(() => {
-    debounce(lint, 250)();
-  });
+    debouncedLint();
+  }, [code, config, syntax]);
 
   return (
     <Linter