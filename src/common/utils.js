7ec4e4d9ce35cf93054ee13bc382761c0a2f3db1
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-09-13 16:34:51 +0300

    create one hash for code, syntax, config

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,61 +1,21 @@
 import LZString from "lz-string";
 
-export function parseQuery(queryString) {
-  const pairs = (queryString[0] === "?"
-    ? queryString.substr(1)
-    : queryString
-  ).split("&");
-
-  return pairs.reduce((queryObj, pair) => {
-    const [key, value] = pair.split("=");
-
-    if (!key) {
-      return queryObj;
-    }
-
-    queryObj[decodeURIComponent(key)] = decodeURIComponent(value || "");
-
-    return queryObj;
-  }, {});
-}
-
-export function stringifyQuery(queryObj) {
-  return Object.keys(queryObj)
-    .map(
-      key => `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key])}`
-    )
-    .join("&");
-}
-
 export function compress(data) {
-  const query = {};
-
   try {
-    Object.keys(data).forEach(
-      key => (query[key] = LZString.compressToEncodedURIComponent(data[key]))
-    );
+    return LZString.compressToEncodedURIComponent(JSON.stringify(data));
   } catch (e) {
     // return silently
     return "";
   }
-
-  return "?" + stringifyQuery(query);
 }
 
 export function decompress(str) {
-  const queryObj = parseQuery(str);
-
   try {
-    Object.keys(queryObj).forEach(
-      key =>
-        (queryObj[key] = LZString.decompressFromEncodedURIComponent(
-          queryObj[key]
-        ))
-    );
+    const data = JSON.parse(LZString.decompressFromEncodedURIComponent(str));
+
+    return typeof data !== "object" || data === null ? {} : data;
   } catch (e) {
     // return silently
     return {};
   }
-
-  return queryObj;
 }
