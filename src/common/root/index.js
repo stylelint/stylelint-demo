77a3b444096e3f88a8a077aecd6c8af14e253610
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-09-13 17:13:29 +0200

    Add ability to share REPL using link (#176)
    * feat: added ability to share REPL using link
    
    * feat: posting message with current query string to every parent
    
    * apply discussions, remove handleDataChange in favor of debounce callback
    
    * create one hash for code, syntax, config
    
    * remove unused variables

--- src/common/root/index.js
+++ src/common/root/index.js
@@ -1,20 +1,35 @@
 /* global fetch:false */
+/* global window:true */
 import React, { useState, useEffect } from "react";
 import Linter from "../linter";
 import recommendedConfig from "stylelint-config-recommended";
 import standardConfig from "stylelint-config-standard";
+import { compress, decompress } from "../utils";
 import "whatwg-fetch";
 import { useDebouncedCallback } from "use-debounce";
 
+const inputDelayMs = 250;
 const defaultCSS = "a {color: #FFF; }\n";
+const defaultSyntax = "css";
 const defaultConfig = {
   rules: Object.assign(recommendedConfig.rules, standardConfig.rules)
 };
 
+const hashData = window.location.hash.slice(
+  window.location.hash.indexOf("#") + 1
+);
+const {
+  code: codeQueryParam,
+  syntax: syntaxQueryParam,
+  config: configQueryParam
+} = decompress(hashData);
+
 export default function Root() {
-  const [code, setCode] = useState(defaultCSS);
-  const [config, setConfig] = useState(JSON.stringify(defaultConfig, null, 2));
-  const [syntax, setSyntax] = useState("css");
+  const [code, setCode] = useState(codeQueryParam || defaultCSS);
+  const [config, setConfig] = useState(
+    configQueryParam ? configQueryParam : JSON.stringify(defaultConfig, null, 2)
+  );
+  const [syntax, setSyntax] = useState(syntaxQueryParam || defaultSyntax);
   const [warnings, setWarnings] = useState([]);
   const [error, setError] = useState(false);
 
@@ -49,7 +64,14 @@ export default function Root() {
 
   const [debouncedLint] = useDebouncedCallback(() => {
     lint();
-  }, 250);
+    const query = compress({ code, syntax, config });
+
+    window.location.hash = query;
+
+    if (window.parent) {
+      window.parent.postMessage(query, "*");
+    }
+  }, inputDelayMs);
 
   useEffect(() => {
     debouncedLint();
