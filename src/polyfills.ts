8d6059f1fdef796bb76eb94e099324147ad20c32
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-04-23 12:02:35 +0900

    Suppress TS error

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,4 @@
 // TODO: This polyfill will be no longer needed when Firefox supports `:has()`. See https://caniuse.com/css-has
+// @ts-expect-error -- TS2307: Cannot find module 'css-has-pseudo/browser' or its corresponding type declarations.
 import cssHasPseudo from 'css-has-pseudo/browser';
 cssHasPseudo(document);
