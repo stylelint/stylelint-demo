b35542551ed0d74485ffc90319db131a82a7d50e
Richard Hallows <jeddy3@users.noreply.github.com>
2025-11-25 12:42:57 +0000

    Add e2e tests for happy paths (#513)
    Co-authored-by: Masafumi Koba <473530+ybiquitous@users.noreply.github.com>

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './tests',
+	timeout: 60_000,
+	reporter: 'html',
+	use: {
+		baseURL: 'http://localhost:5173',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
+	],
+	webServer: {
+		command: 'npm run dev',
+		url: 'http://localhost:5173',
+	},
+});
